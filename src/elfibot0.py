8c7a54fdd36fa8f5204e2100753ade327b867b4e
Alexander Elfimow <my.free.mail@gmx.de>
2017-10-22 19:02:51 +0200

    Simple message handler

--- src/elfibot0.py
+++ src/elfibot0.py
@@ -1,5 +1,13 @@
+import time
 import telepot
+from telepot.loop import MessageLoop
 from pprint import pprint
+
+def elfibot0_msg_handler(msg):
+    content_type, chat_type, chat_id = telepot.glance(msg)
+    print(content_type, chat_type, chat_id)
+    if content_type == 'text':
+        elfibot0.sendMessage(chat_id, msg['text'])
 
 def elfibot0_main():
     global elfibot0
@@ -17,6 +25,11 @@ def elfibot0_main():
 
     elfibot0_Info = elfibot0.getMe()
     pprint(elfibot0_Info)
+
+    MessageLoop(elfibot0, elfibot0_msg_handler).run_as_thread()
+
+    while 1:
+        time.sleep(10)
 
 if __name__ == '__main__':
     elfibot0_main()
